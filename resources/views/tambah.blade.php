fa85d21da78f38d311513dd8b03f20d3ca12a098
Artha Maulana <arthamaulana2005@gmail.com>
2025-06-04 12:29:54 +0700

    pertemuan 14

---
+++ resources/views/tambah.blade.php
@@ -0,0 +1,58 @@
+@extends('template')
+
+@section('content')
+	<h3>Data Pegawai</h3>
+
+	<a href="/pegawai" class="btn btn-info"> Kembali</a>
+
+	<br/>
+	<br/>
+
+	<form action="/pegawai/store" method="post">
+		{{ csrf_field() }}
+        <div class="row">
+            <div class="col-3">
+                Nama
+            </div>
+            <div class="col-8">
+                <input type="text" required="required" name="nama" class="form-control">
+            </div>
+        </div>
+        <br />
+        <div class="row">
+            <div class="col-3">
+                Jabatan
+            </div>
+            <div class="col-8">
+                <input type="text" required="required" name="jabatan" class="form-control">
+            </div>
+        </div>
+        <br />
+        <div class="row">
+            <div class="col-3">
+                Umur
+            </div>
+            <div class="col-8">
+                <input type="number" required="required" name="umur" class="form-control">
+            </div>
+        </div>
+        <br />
+        <div class="row">
+            <div class="col-3">
+                Alamat
+            </div>
+            <div class="col-8">
+                <textarea required="required" name="alamat" class="form-control"></textarea>
+            </div>
+        </div>
+        <br />
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-8">
+                <input type="submit" value="Simpan Data" class="btn btn-success">
+            </div>
+        </div>
+
+	</form>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\Link;
 use App\Http\Controllers\Pegawai2Controller;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\PegawaiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,45 +17,63 @@ use App\Http\Controllers\PegawaiController;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('bootstrap1', function () {
-    return view('bootstrap1');
-});
-
-Route::get('index', function () {
-    return view('index');
-});
-
-Route::get('pertama', function () {
-    return view('pertama');
-});
-
-Route::get('PERTEMUAN4', function () {
-    return view('PERTEMUAN4');
-});
-
-Route::get('pseudo-elements&grid counter', function () {
-    return view('pseudo-elements&grid counter');
-});
-
-Route::get('frontend', function () {
     return view('frontend');
 });
 
-Route::get('dosen', [Link::class, 'index'] );
 
-//Route::get('/pegawai/{nama}', [Pegawai2Controller::class, 'index'] );
+Route::get('/index', function () {
+    return view('index');
+});
 
-Route::get('/formulir', [PegawaiController::class,'formulir'] );
-Route::post('/formulir/proses', [PegawaiController::class,'proses'] );
+Route::get('/js1', function () {
+    return view('js1');
+});
 
-// route blog
-Route::get('/blog' , [BlogController::class, 'home']);
+Route::get('/js2', function () {
+    return view('js2');
+});
+
+Route::get('/bootstrap', function () {
+    return view('bootstrap');
+});
+
+Route::get('/latihanlayout', function () {
+    return view('latihanlayout');
+});
+
+Route::get('/linktree', function () {
+    return view('linktree');
+});
+
+Route::get('/pertama', function () {
+    return view('pertama');
+});
+
+Route::get('/welcome.blade', function () {
+    return view('welcome.blade');
+});
+
+Route::get('/biodata.blade', function () {
+    return view('biodata.blade');
+});
+
+Route::get('/dosen', [Link::class,'index']);
+// Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+
+Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
-Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+Route::get('/blog/kontak', [BlogController::class,'kontak']);
 
-//crud
-Route::get('/pegawai', [PegawaiController::class, 'index'] );
+Route::get('/pegawai', [PegawaiController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiController::class,'tambah'] );
+Route::post('/pegawai/store', [PegawaiController::class,'store'] );
+Route::get('/pegawai/edit/{id}', [PegawaiController::class,'edit'] );
+Route::post('/pegawai/update', [PegawaiController::class,'update'] );
+Route::get('/pegawai/hapus/{id}', [PegawaiController::class,'hapus'] );
+Route::get('/pegawai/cari', [PegawaiController::class, 'cari']);
