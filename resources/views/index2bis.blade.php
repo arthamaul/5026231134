da215cd0209cb9733c878e198d201db33ce95cde
Artha Maulana <arthamaulana2005@gmail.com>
2025-06-11 12:07:04 +0700

    tugas pertemuan 14

--- resources/views/index2bis.blade.php
+++ resources/views/index2bis.blade.php
@@ -1,7 +1,7 @@
 @extends('template')
 
 @section('content')
-    <h3>Data Kartu Grafis (BIS)</h3>
+    <h3>Data (BIS)</h3>
 
     {{-- Tombol Tambah BIS Baru (sekarang tidak dalam komentar) --}}
     <a href="/bis/tambah" class="btn btn-primary"> + Tambah Data BIS Baru</a>
